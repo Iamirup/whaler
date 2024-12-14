e8aad70f81537618d519294ede549f15c1c3a007
AmirHossein Hafizi <wantedamir897@gmail.com>
2024-12-14 18:31:58 +0330

    new

--- frontend/src/components/refreshJWT.ts
+++ frontend/src/components/refreshJWT.ts
@@ -1,14 +1,10 @@
 import axios from 'axios';
-import { useRouter } from 'vue-router';
 import { alertService } from './alertor';
-
-const router = useRouter();
 
 export const refreshJWT = async (): Promise<boolean> => {
     await axios.get("/api/auth/v1/refresh")
     .then(response => {})
     .catch(error => {
-        console.error(error);
         if (error.response.data.need_login){
             alertService.showAlert("You need to login bro 🎩", "info");
             return false;
--- frontend/src/views/Blog.vue
+++ frontend/src/views/Blog.vue
@@ -8,7 +8,6 @@
 import ArticleList from '../components/blog/ArticleList.vue';
 import { refreshJWT } from '../components/refreshJWT';
 import { useRouter } from 'vue-router';
-import { alertService } from '../components/alertor';
 import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
@@ -18,6 +17,7 @@ export default defineComponent({
 
     onMounted(async () => {
       const isThereUser = await refreshJWT(); 
+      console.log("isThereUser: ", isThereUser);
       if (!isThereUser) { 
         router.push('/login'); 
       } else {
