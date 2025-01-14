0fd999f960974384c8dc52d2666ee30afc588cad
AmirHossein Hafizi <wantedamir897@gmail.com>
2025-01-14 15:08:31 +0330

    new service d

--- backend/microservices/discussion/internal/core/domain/entity/comment.go
+++ backend/microservices/discussion/internal/core/domain/entity/comment.go
@@ -6,7 +6,7 @@ type UUID string
 
 type Comment struct {
 	CommentId int64     `json:"comment_id"`
-	TopicId   UUID      `json:"user_id"`
+	TopicId   int64     `json:"user_id"`
 	Currency  string    `json:"currency"`
 	Username  string    `json:"username"`
 	Text      string    `json:"text"`
